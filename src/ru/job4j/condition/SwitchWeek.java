512f6d28c6767160cd0457353043295394206948
Markelov Roman <roman.sercent@gmail.com>
2020-09-23 11:09:41 +0300

    4.6.1. Switch [#369457]

---
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import java.util.Random;
+
+public class SwitchWeek {
+    public String nameOfDay(int day) {
+        String name;
+        switch (day) {
+            case 1:
+                name = "Monday";
+            case 2:
+                name = "Tuesday";
+            case 3:
+                name = "Wednesday";
+            case 4:
+                name = "Thursday";
+            case 5:
+                name = "Friday";
+            case 6:
+                name = "Saturday";
+            case 7:
+                name = "Sunday";
+            default:
+                name = "Error";
+        }
+        return name;
+    }
+}
