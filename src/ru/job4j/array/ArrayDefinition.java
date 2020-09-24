575778b08d24d5936f7eea1391e377464cfb589a
Markelov Roman <roman.sercent@gmail.com>
2020-09-24 11:59:48 +0300

    6.0.2. Заполнение массива. [#369481]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,10 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surmanes = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
         System.out.println("The size of the array ages = " + ages.length);
         System.out.println("The size of the array surmanes = " + surmanes.length);
         System.out.println("The size of the array prices = " + prices.length);
+        names[0] = "Ivan";
+        names[1] = "Lavrentyi";
+        names[2] = "Fred";
+        names[3] = "Karl";
 
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+
+        }
     }
 
 }
