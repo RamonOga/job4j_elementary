8e61e3d9f6f43e42113fa0003c0845c7ad3ce865
Markelov Roman <roman.sercent@gmail.com>
2020-09-24 11:53:13 +0300

    6.0.1.1. Размер массива [#369492]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,10 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surmanes = new String[100500];
         float[] prices = new float[40];
+        System.out.println("The size of the array ages = " + ages.length);
+        System.out.println("The size of the array surmanes = " + surmanes.length);
+        System.out.println("The size of the array prices = " + prices.length);
+
     }
 
 }
