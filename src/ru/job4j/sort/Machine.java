08742ec438429ca7cdfa692ab814fe2b34457fcb
Markelov Roman <roman.sercent@gmail.com>
2020-09-27 19:11:43 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#369468]

---
+++ src/ru/job4j/sort/Machine.java
@@ -0,0 +1,21 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] coins = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        money -= price;
+        for (int i = 0; i < coins.length; i++) {
+            while (money >= coins[i]) {
+                rsl[size] = coins[i];
+                size++;
+                money -= coins[i];
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
