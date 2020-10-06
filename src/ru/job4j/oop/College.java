9d7980a97ed5c7e19fad810d54dab9091373fb71
Markelov Roman <roman.sercent@gmail.com>
2020-10-06 17:40:23 +0300

    3. Интерфейс Input [#369610]

---
+++ src/ru/job4j/oop/College.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class College {
+
+    Freshman newb = new Freshman();
+    Student stu = newb;
+    Object object = stu;
+
+
+}
---
+++ src/ru/job4j/oop/HierarchyUsage.java
@@ -0,0 +1,6 @@
+package ru.job4j.oop;
+
+public class HierarchyUsage {
+    public static void main(String[] args) {
+    }
+}
