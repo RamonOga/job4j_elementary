c2186af0609125aa0b91b6fba1b37dc29e865f37
Markelov Roman <roman.sercent@gmail.com>
2020-10-08 21:41:44 +0300

    0.4. Пользовательские исключения. [#369630]

---
+++ src/ru/job4j/ex/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            } else {
+                throw new ElementNotFoundException();
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"qwe", "eqw"};
+        try {
+            indexOf(array, "1231");
+        } catch (ElementNotFoundException e) {
+            e.fillInStackTrace();
+        }
+    }
+}
