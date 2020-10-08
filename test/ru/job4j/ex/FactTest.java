611ef17cbf6d5ef1e5da2db231e289903f80f556
Markelov Roman <roman.sercent@gmail.com>
2020-10-08 20:59:42 +0300

    0.3. Тестирование исключений с junit [#369629]

---
+++ test/ru/job4j/ex/FactTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void thenLessZero() {
+        Fact fact = new Fact();
+        fact.calc(-12);
+    }
+
+}
